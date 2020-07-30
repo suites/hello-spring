357237c5c70aeff5d6a5d0a5bc8a47ea338639b7
suites <woosiks.io@gmail.com>
2020-07-31 02:11:48 +0900

    [controller] api controller 작성

--- src/main/kotlin/hello/hellospring/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospring/controller/HelloController.kt
@@ -2,8 +2,7 @@ package hello.hellospring.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class HelloController {
@@ -18,4 +17,18 @@ class HelloController {
         model.addAttribute("name", name)
         return "hello-template"
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam("name") name: String): String {
+        return "hello$name"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam("name") name: String): Hello {
+        return Hello(name)
+    }
+
+    class Hello(val name: String)
 }
