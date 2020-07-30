db6bc2ea39e9eafa62a4b03f4e2b49b59f839036
suites <woosiks.io@gmail.com>
2020-07-31 00:01:31 +0900

    [controller] hello-template 작성

--- src/main/kotlin/hello/hellospring/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospring/controller/HelloController.kt
@@ -3,6 +3,7 @@ package hello.hellospring.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HelloController {
@@ -10,5 +11,11 @@ class HelloController {
     fun hello(model: Model): String {
         model.addAttribute("data", "hello!!")
         return "hello"
+    }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "hello-template"
     }
 }
