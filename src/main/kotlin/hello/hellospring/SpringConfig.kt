d874980255dc7abff7589d0f7bc0566665eb8f30
suites <woosiks.io@gmail.com>
2020-08-03 02:34:41 +0900

    [config] 코드 정리

--- src/main/kotlin/hello/hellospring/SpringConfig.kt
+++ src/main/kotlin/hello/hellospring/SpringConfig.kt
@@ -1,11 +1,9 @@
 package hello.hellospring
 
-import hello.hellospring.repository.JpaMemberRepository
 import hello.hellospring.repository.MemberRepository
 import hello.hellospring.service.MemberService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.persistence.EntityManager
 
 @Configuration
 class SpringConfig(private val memberRepository: MemberRepository) {
