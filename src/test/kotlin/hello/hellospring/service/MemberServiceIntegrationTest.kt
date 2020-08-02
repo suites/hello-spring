c7b76d6444548e4c6afa419c4d5e5ce81d177e03
suites <woosiks.io@gmail.com>
2020-08-02 16:00:53 +0900

    [service] 통합 테스트 작성

---
+++ src/test/kotlin/hello/hellospring/service/MemberServiceIntegrationTest.kt
@@ -0,0 +1,46 @@
+package hello.hellospring.service
+
+import hello.hellospring.domain.Member
+import hello.hellospring.repository.MemberRepository
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import java.lang.IllegalStateException
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest(
+        @Autowired private val memberService: MemberService,
+        @Autowired private val memberRepository: MemberRepository
+) {
+
+    @Test
+    fun 회원가입() {
+        // given
+        val member = Member(name = "hello")
+
+        // when
+        val saveId = memberService.join(member)
+
+        // then
+        val findMember = memberRepository.findById(saveId)
+        assertThat(member.name).isEqualTo(findMember?.name)
+    }
+
+    @Test
+    fun 중복_회원_가입_예외() {
+        // given
+        val member1 = Member(name = "hello")
+        val member2 = Member(name = "hello")
+
+        // when
+        memberService.join(member1)
+        val e = assertThrows<IllegalStateException> { memberService.join(member2) }
+
+        // then
+        assertThat(e.message).isEqualTo("이미 존재하는 회원입니다.")
+    }
+}
